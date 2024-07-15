d26878f025456cedb22c997b5aa023e52ba2d776
D1204493 <161418128+D1204493@users.noreply.github.com>
2024-07-15 12:01:14 +0800

    Summary_0715

---
+++ BMI/app/src/main/java/com/example/bmi/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.bmi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> intentActivityResultLanucher;
+    private double bmi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        intentActivityResultLanucher = registerForActivityResult (
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        //寫另一個Activity回傳後，得到回傳的資料之後的做法
+                        if(result.getData() != null && result.getResultCode() == Activity.RESULT_OK) {
+                            bmi = result.getData().getDoubleExtra("BMI",-1);
+                        }
+
+                    }
+                }
+        );
+
+    }
+
+    public void GotoBMICalculate(View view) {
+        Intent intent = new Intent(this, CalBMIActivity.class);
+
+
+    }
+
+
+
+}
