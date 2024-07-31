095426d89eb98367096ff72f057e0f7c33058822
D1204493 <161418128+D1204493@users.noreply.github.com>
2024-07-31 10:49:57 +0800

    EvenDemo_teacher範例_0731

--- EvenDemo_0731/app/build.gradle.kts
+++ EvenDemo_0731/app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.layoutpractise"
+    namespace = "com.example.evendemo_0731"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.layoutpractise"
+        applicationId = "com.example.evendemo_0731"
         minSdk = 30
         targetSdk = 34
         versionCode = 1
@@ -37,14 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
     testImplementation(libs.junit)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
